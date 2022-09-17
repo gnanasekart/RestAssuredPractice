3cd9a2f179943a11e12b4ef44faf1aedf54e5fc8
t.gnanasekar <gnanasekar69@hotmail.com>
2022-09-17 19:38:37 +0530

    new file

--- src/test/java/AnnotationsJackson/EmployeePojo.java
+++ src/test/java/AnnotationsJackson/EmployeePojo.java
@@ -5,7 +5,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 public class EmployeePojo {
 
-
     private String FirstName;
     private String LastName;
     @JsonInclude(JsonInclude.Include.NON_NULL)
